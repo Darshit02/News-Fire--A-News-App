c1e8d1358b9f77bcc33b6441d1f31b34476b0bfd
Darshit <darshitmistry2@gmail.com>
2022-12-15 21:03:09 +0530

    upgrede some functionality

--- src/components/News.js
+++ src/components/News.js
@@ -31,7 +31,7 @@ const News = (props)=>{
     }
 
     useEffect(() => {
-         document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+         document.title = `${capitalizeFirstLetter(props.category)} - NewsFire`;
         updateNews(); 
     }, [])
  
